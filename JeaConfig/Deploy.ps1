96aece905dd278562180d02d47ea702b50b66705
Raimund Andree <r.andree@live.com>
2021-03-25 19:11:11 +0100

    Using Force

--- JeaConfig/Deploy.ps1
+++ JeaConfig/Deploy.ps1
@@ -2,4 +2,4 @@ $here = $PSScriptRoot
 dir -Path $here\BuildOutput\MOF | Where-Object Name -NotLike 'jWeb1*' | Remove-Item
 
 Set-DscLocalConfigurationManager -Path $here\BuildOutput\MetaMof -Verbose
-Start-DscConfiguration -Path $here\BuildOutput\MOF -Wait -Verbose
+Start-DscConfiguration -Path $here\BuildOutput\MOF -Force -Wait -Verbose
